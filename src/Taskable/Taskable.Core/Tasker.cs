cfe98ccd0c4d03435cc78b9c9b609809c389c795
Karthik Ananthapadmanaban <karthika25@gmail.com>
2016-07-23 21:33:04 -0400

    tasker & a singleton for the same

---
+++ src/Taskable/Taskable.Core/Tasker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskableCore.Concrete;
+using TaskableCore.Exceptions;
+using TaskableCore.Extensions;
+using TaskableScriptCs.Contracts;
+
+namespace TaskableCore
+{
+    public sealed class Tasker
+    {
+        private static volatile Tasker tasker;
+        private static object rootObj = new Object();
+
+        private List<ComputedTask> _rawTasks;
+        private ILookup<string, ComputedTask> _taskLookup;
+        private bool _isInitialized = false;
+
+        private Tasker()
+        {
+            _rawTasks = new List<ComputedTask>();
+        }
+
+        public static Tasker Instance
+        {
+            get
+            {
+                if (tasker == null)
+                {
+                    lock (rootObj)
+                    {
+                        if (tasker == null)
+                        {
+                            tasker = new Tasker();
+                        }
+                    }
+                }
+                return tasker;
+            }
+        }
+
+        public void RegisterTask(ISimpleTask simpleTask)
+        {
+            if (_isInitialized)
+                throw new NotSupportedException("You cannot register tasks after initializing taskable.");
+
+            TaskValidator.ValidateTask(simpleTask);
+
+            var computedTask = new ComputedTask(simpleTask);
+
+            if (_rawTasks.Any(t => t.Name == computedTask.Name))
+                throw new DuplicateTaskDefinitionException(computedTask.Name);
+
+            Console.WriteLine("[General] Registered a task: {0}", computedTask.Name);
+            _rawTasks.Add(computedTask);
+        }
+
+        public bool InvokeTask(string command)
+        {
+            var task = _taskLookup.FindTask(command);
+            if (task != null)
+            {
+                var parameters = task.GetParameters(command);
+                task.Stuff(parameters.ToArray());
+                return true;
+            }
+            return false;
+        }
+
+        public void Initialize()
+        {
+            if (!_isInitialized)
+            {
+                _taskLookup = _rawTasks.ToLookup(k => k.Pattern.Shift(), v => v);
+                _isInitialized = true;
+                Console.WriteLine("[General] Initialized 'taskable'");
+            }
+        }
+    }
+}
--- src/Taskable/TaskableScriptPack/Taskable.cs
+++ src/Taskable/TaskableScriptPack/Taskable.cs
@@ -1,79 +1,39 @@
 ﻿using System;
-using System.Linq;
 using ScriptCs.Contracts;
 using System.Collections.Generic;
 using TaskableCore;
-using TaskableCore.Concrete;
 using TaskableCore.Extensions;
-using TaskableCore.Exceptions;
 using TaskableScriptCs.Contracts;
 
 namespace TaskableScriptPack
 {
     public class Taskable : IScriptPackContext, ITaskableContext
     {
-        private List<ComputedTask> _rawTasks;
-        private ILookup<string, ComputedTask> _taskLookup;
-
-        private bool _isInitialized = false;
-
+        private Tasker _tasker;
         private readonly Options _options;
 
         public Taskable(Options options)
         {
             _options = options;
-            _rawTasks = new List<ComputedTask>();
+            _tasker = Tasker.Instance;
         }
 
         public void WaitForCommands()
         {
-            Initialize();
+            _tasker.Initialize();
             "Enter 'bye' or 'q' to exit from the prompt.".PrintGreen();
             _options.ReplPrefix.Print();
             string line = null;
             while (!ExitSignals.Contains(line = Console.ReadLine()))
             {
-                var status = InvokeTask(line);
+                var status = _tasker.InvokeTask(line);
                 _options.ReplPrefix.Print();
             }
         }
 
         public void RegisterTask(ISimpleTask simpleTask)
         {
-            if (_isInitialized)
-                throw new NotSupportedException("You cannot register tasks after initializing taskable.");
-
-            TaskValidator.ValidateTask(simpleTask);
-
-            var computedTask = new ComputedTask(simpleTask);
-
-            if (_rawTasks.Any(t => t.Name == computedTask.Name))
-                throw new DuplicateTaskDefinitionException(computedTask.Name);
-
-            Console.WriteLine("[General] Registered a task: {0}", computedTask.Name);
-            _rawTasks.Add(computedTask);
-        }
-
-        private bool InvokeTask(string command)
-        {
-            var task = _taskLookup.FindTask(command);
-            if (task != null)
-            {
-                var parameters = task.GetParameters(command);
-                task.Stuff(parameters.ToArray());
-                return true;
-            }
-            return false;
-        }
-
-        private void Initialize()
-        {
-            if (!_isInitialized)
-            {
-                _taskLookup = _rawTasks.ToLookup(k => k.Pattern.Shift(), v => v);
-                _isInitialized = true;
-                Console.WriteLine("[General] Initialized 'taskable'");
-            }
+            _tasker.RegisterTask(simpleTask);
         }
 
         private static List<string> ExitSignals = new List<string> { "bye", "q" };
