908f7d680288e03441968da3d484290c960f8ad2
Karthik Ananthapadmanaban <karthika25@gmail.com>
2016-07-31 00:07:12 -0400

    started using the tasker

--- src/Taskable/Taskable/ViewModels/TaskSelectorViewModel.cs
+++ src/Taskable/Taskable/ViewModels/TaskSelectorViewModel.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using TaskableCore;
 
 namespace TaskableApp.ViewModels
 {
     public class TaskSelectorViewModel : BindableBase
     {
+        private Tasker _tasker;
 
+        public List<string> CommandList
+        {
+            get;set;
+        }
+
+        public TaskSelectorViewModel()
+        {
+            _tasker = Tasker.Instance;
+            this.CommandList = new List<string>
+            {
+                "echo",
+                "git-download",
+                "git-download-s"
+            };
+        }
     }
 }
--- src/Taskable/Taskable/Views/TaskSelectorView.xaml.cs
+++ src/Taskable/Taskable/Views/TaskSelectorView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TaskableApp.ViewModels;
 
 namespace TaskableApp.Views
 {
@@ -23,6 +12,7 @@ namespace TaskableApp.Views
         public TaskSelectorView()
         {
             InitializeComponent();
+            this.DataContext = new TaskSelectorViewModel();
         }
 
         private void BtnSettings_Click(object sender, RoutedEventArgs e)
