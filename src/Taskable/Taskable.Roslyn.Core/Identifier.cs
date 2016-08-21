cf531ef5bad2928ef5a731f4869c4a8ec291fe38
Karthik Ananthapadmanaban <karthika25@gmail.com>
2016-08-21 01:54:42 -0400

    almost there for id list

--- src/Taskable/Taskable.Roslyn.Core/Identifier.cs
+++ src/Taskable/Taskable.Roslyn.Core/Identifier.cs
@@ -5,6 +5,22 @@
         public IdentifierType Type { get; set; }
         public string FullName { get; set; }
         public int LineNumber { get; set; }
+
+        public string Image
+        {
+            get
+            {
+                switch (Type)
+                {
+                    case IdentifierType.Class:
+                    case IdentifierType.Method:
+                    case IdentifierType.Property:
+                    case IdentifierType.Field:
+                    default:
+                        return "/Images/vscode.png";
+                }
+            }
+        }
     }
 
     public enum IdentifierType
