c18a4cac59ce12892803e4c13d48ebc4653dfba3
karthik25 <karthika25@gmail.com>
2016-09-30 21:53:48 -0400

    working sample task

--- src/Taskable/Taskable.SampleTasks/DownloadFromGithub.cs
+++ src/Taskable/Taskable.SampleTasks/DownloadFromGithub.cs
@@ -1,23 +1,40 @@
 ﻿using System;
+using System.Net;
 using TaskableScriptCs.Contracts;
 
 namespace TaskableSampleTasks
 {
-    public class DownloadFromGithub : ISimpleTask
+    [TaskName("Download Task")]
+    [TaskExample(@"git-download https://github.com/karthik25/sblog.net/archive/master.zip to d:\temp\GitDownloads")]
+    public class GitDownloadTask : ISimpleTask
     {
         public string Pattern
         {
             get
             {
-                return "download {} to {}";
+                return "git-download {} to {}";
             }
         }
 
-        Action<string[]> ISimpleTask.Stuff
+        public Action<string[]> Stuff
         {
             get
             {
-                throw new NotImplementedException();
+                return parameters =>
+                {
+                    var fileUrl = parameters[0];
+                    var destinationDirectory = parameters[1];
+                    TaskProgress.Report("Copying " + fileUrl + " to " + destinationDirectory);
+                    try
+                    {
+                        var client = new WebClient();
+                        client.DownloadFile(fileUrl, destinationDirectory + @"\master.zip");
+                    }
+                    catch (Exception ex)
+                    {
+                        TaskProgress.ReportError(ex.Message.ToString());
+                    }
+                };
             }
         }
     }
